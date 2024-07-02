f83c698a584263e1fc883024323308ae21a8eecc
edac99 <jamjam1208@naver.com>
2024-07-03 00:57:48 +0900

    modify: 랜딩 UI 수정
    규격 수정

--- client/src/pages/Main/index.tsx
+++ client/src/pages/Main/index.tsx
@@ -1,4 +1,4 @@
-import posterImage from "../../assets/images/poster.png";
+import posterImage from "../../assets/images/poster.webp";
 import WideButton from "../../components/WideButton";
 import theme from "../../styles/theme";
 import { PosterImage } from "./style";
--- client/src/pages/Main/style.ts
+++ client/src/pages/Main/style.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const PosterImage = styled.img`
-  margin-top: 6rem;
+  margin-top: 4rem;
   width: 90vw;
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
 `;
