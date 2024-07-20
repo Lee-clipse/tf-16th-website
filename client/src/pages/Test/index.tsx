4fdd03bebd67963b62386c2a925dcd74ddaf1413
edac99 <jamjam1208@naver.com>
2024-07-20 21:19:58 +0900

    feat: 스크롤 페이드 애니메이션 구현
    테스트 페이지

--- client/src/pages/Test/index.tsx
+++ client/src/pages/Test/index.tsx
@@ -3,20 +3,30 @@ import CountUp from "react-countup";
 import VisibilitySensor from "react-visibility-sensor";
 import styled from "styled-components";
 import theme from "../../styles/theme";
+import InstaLogo from "../../assets/images/insta_logo.webp";
 
 const TestPage = () => {
   return (
-    <div className="col-lg-3 col-md-6">
-      <div className="stats-item text-center w-100 h-100">
-        <VisibilitySensor partialVisibility offset={{ bottom: 200 }}>
-          {({ isVisible }: { isVisible: boolean }) => (
-            <CountUpItem className="f-row">
-              {isVisible ? <CountUp start={0} end={300} duration={1} /> : null}
-              <p>명의 스텝</p>
-            </CountUpItem>
-          )}
-        </VisibilitySensor>
-      </div>
+    <div>
+      <div style={{ backgroundColor: "palegreen", height: "500px" }}></div>
+
+      <CountUpItem className="f-row">
+        <CountUp start={0} end={300} duration={1} />
+        <p>명의 스텝</p>
+      </CountUpItem>
+
+      <div style={{ backgroundColor: "palegreen", height: "500px" }}></div>
+
+      <VisibilitySensor partialVisibility offset={{ bottom: 200 }}>
+        {({ isVisible }: { isVisible: boolean }) => (
+          <FadeInItem className={`f-row ${isVisible ? "visible" : "hidden"}`}>
+            {isVisible ? <img src={InstaLogo} /> : null}
+            <p>-</p>
+          </FadeInItem>
+        )}
+      </VisibilitySensor>
+
+      <div style={{ backgroundColor: "palegreen", height: "500px" }}></div>
     </div>
   );
 };
@@ -24,6 +34,15 @@ const TestPage = () => {
 const CountUpItem = styled.div`
   font-family: ${theme.font.NOTO[8]};
   font-size: ${theme.font.SIZE.XXL};
+`;
+
+const FadeInItem = styled.div`
+  opacity: 0;
+  transition: opacity 1s ease-in-out;
+  height: 81px;
+  &.visible {
+    opacity: 1;
+  }
 `;
 
 export default TestPage;
