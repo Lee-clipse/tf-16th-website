8b08199201a1560237824b5283f7cba1f8e118a1
Lee-clipse <79911816+Lee-clipse@users.noreply.github.com>
2024-08-09 00:46:55 +0900

    feat: 인트로 만화 재생 구현

--- client/src/pages_zerogame/Cartoon/index.tsx
+++ client/src/pages_zerogame/Cartoon/index.tsx
@@ -1,7 +1,34 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useEffect, useState } from "react";
 import { Wrapper } from "./style";
+import ZGCartoon from "../../assets/images/zg_intro_cartoon.png";
+import { useNavigate } from "react-router-dom";
+import { API_CODE, ROUTE_PATH } from "../../common/const";
+import { reqGameEnter } from "../../api/zerogame";
+import { getUserIdByToken } from "../../common/common";
 
 const ZGCartoonPage = () => {
-  return <Wrapper>index</Wrapper>;
+  const navigate = useNavigate();
+
+  const handleStartButton = async () => {
+    const userId = getUserIdByToken().toString();
+    const res = await reqGameEnter({ userId });
+    const ok = Number(res.data.code) === API_CODE.SUCCESS;
+    if (ok) {
+      navigate(ROUTE_PATH.ZG_HOME);
+    }
+  };
+
+  return (
+    <>
+      <Wrapper>
+        <img id="bg" src={ZGCartoon} />
+        <div id="start-btn" onClick={() => handleStartButton()}>
+          출발하기
+        </div>
+      </Wrapper>
+    </>
+  );
 };
 
 export default ZGCartoonPage;
--- client/src/pages_zerogame/Cartoon/style.ts
+++ client/src/pages_zerogame/Cartoon/style.ts
@@ -1,3 +1,31 @@
 import styled from "styled-components";
+import theme from "../../styles/theme";
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+
+  #bg {
+    width: 100%;
+    height: 100vh;
+    object-fit: cover;
+    z-index: -1;
+    position: absolute;
+  }
+
+  #start-btn {
+    background-color: ${theme.color.ORANGE};
+    border-radius: 0.6rem;
+    padding: 1rem;
+    color: ${theme.color.WHITE};
+    font-family: ${theme.font.NOTO[7]};
+    font-size: ${theme.font.SIZE.XXXL};
+    position: absolute;
+    text-align: center;
+    top: 86%;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 90vw;
+  }
+`;
