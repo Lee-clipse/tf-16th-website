0f888a548da9af819e0601ff19e1c1c0f57ea7bb
Lee-clipse <79911816+Lee-clipse@users.noreply.github.com>
2024-08-09 02:07:49 +0900

    feat: 제로게임 부스 목록 페이지 layout 구현

--- client/src/pages_zerogame/Booth/index.tsx
+++ client/src/pages_zerogame/Booth/index.tsx
@@ -1,7 +1,68 @@
-import { Wrapper } from "./style";
+import { useState } from "react";
+import { Modal, Wrapper } from "./style";
+import { useNavigate } from "react-router-dom";
+import { API_CODE, BOOTH_LIST, ROUTE_PATH } from "../../common/const";
+import { getUserIdByToken } from "../../common/common";
+import CloseIcon from "../../assets/icons/close.png";
+import { reqSelectBooth } from "../../api/zerogame";
 
 const ZGBoothPage = () => {
-  return <Wrapper>index</Wrapper>;
+  const navigate = useNavigate();
+  const [viewBoothModal, setViewBoothModal] = useState<boolean>(false);
+  const [selectBoothId, setSelectBoothId] = useState<string>("0");
+
+  const handleSelectBooth = (boothId: string) => {
+    setSelectBoothId(boothId);
+    setViewBoothModal(true);
+  };
+
+  const handleEnterBooth = async () => {
+    const userId = getUserIdByToken().toString();
+    const res = await reqSelectBooth({ userId, boothId: selectBoothId });
+    console.log(res.data);
+
+    const ok = Number(res.data.code) === API_CODE.SUCCESS;
+    if (ok) {
+      navigate(ROUTE_PATH.ZG_HOME);
+      setViewBoothModal(false);
+    }
+  };
+
+  return (
+    <>
+      {/* 부스 선택 모달 */}
+      {viewBoothModal && (
+        <Modal>
+          <div id="back-drop" onClick={() => setViewBoothModal(false)}></div>
+          <div id="m-wrapper">
+            <div id="m-header">
+              <img src={CloseIcon} onClick={() => setViewBoothModal(false)} />
+            </div>
+            <div id="m-body">
+              <div>boothId: {selectBoothId}</div>
+              <div>{BOOTH_LIST[Number(selectBoothId)]}</div>
+              <div className="b-btn" onClick={() => handleEnterBooth()}>
+                부스 선택
+              </div>
+            </div>
+          </div>
+        </Modal>
+      )}
+
+      {/* TODO: 이미 클리어한 부스에 대해 처리 */}
+      <Wrapper>
+        <div id="b-title">부스 목록</div>
+        <div id="b-list" className="f-col h-center">
+          {Object.entries(BOOTH_LIST).map(([key, value]) => (
+            <div className="b f-row f-spb" key={key} onClick={() => handleSelectBooth(key)}>
+              <div>{value}</div>
+              <div>{">"}</div>
+            </div>
+          ))}
+        </div>
+      </Wrapper>
+    </>
+  );
 };
 
 export default ZGBoothPage;
--- client/src/pages_zerogame/Booth/style.ts
+++ client/src/pages_zerogame/Booth/style.ts
@@ -1,3 +1,84 @@
 import styled from "styled-components";
+import theme from "../../styles/theme";
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+  padding: 3rem 2rem;
+  font-family: ${theme.font.NOTO[5]};
+  font-size: ${theme.font.SIZE.ML};
+
+  #b-title {
+    font-size: ${theme.font.SIZE.XXL};
+    text-align: center;
+    margin-bottom: 4rem;
+  }
+
+  #b-list {
+    height: 70vh;
+    overflow: scroll;
+    border: 2px solid lightgray;
+    border-radius: 1rem;
+
+    .b {
+      border-bottom: 1px solid lightgray;
+      padding: 2rem;
+    }
+  }
+`;
+
+export const Modal = styled.div`
+  #back-drop {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+    z-index: 999;
+  }
+
+  #m-wrapper {
+    z-index: 1000;
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 80%;
+    max-height: 90vh;
+    overflow-y: auto;
+
+    background: ${theme.color.WHITE};
+    border-radius: 1rem;
+  }
+
+  #m-header {
+    display: flex;
+    flex-direction: row;
+    justify-content: right;
+    align-items: center;
+    border-bottom: 1px solid lightgray;
+    padding: 1rem;
+
+    font-size: 1.6rem;
+    font-weight: bold;
+
+    img {
+      width: 5vw;
+    }
+  }
+
+  #m-body {
+    font-family: ${theme.font.NOTO[5]};
+    font-size: ${theme.font.SIZE.ML};
+    padding: 1rem;
+
+    .b-btn {
+      border: 2px solid ${theme.color.BLACK};
+      text-align: center;
+      padding: 1rem;
+      border-radius: 1rem;
+    }
+  }
+`;
