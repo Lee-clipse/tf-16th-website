7f08a84c52b737efbe3cd77b36071e8f67f919b6
Lee-clipse <79911816+Lee-clipse@users.noreply.github.com>
2024-08-31 04:38:14 +0900

    modify: 0831 시뮬 입장 코드

--- client/src/pages_zerogame/Logo/index.tsx
+++ client/src/pages_zerogame/Logo/index.tsx
@@ -62,7 +62,7 @@ const ZGLogoPage = () => {
 
   const handleClickCodeButton = async () => {
     // TODO: 제로게임 접수 부스 입장 코드 (변경 필요)
-    if (zeroCode === "leeclipse") {
+    if (zeroCode === "3456") {
       setViewCodeModal(false);
       setIsCodeValid(true);
       setViewAgreeModal(true);
