7b97b7ceb182d029a1918e2c135972303d107f13
KimCookieYa <min49590@gmail.com>
2024-09-04 01:58:43 +0900

    chore: polyfil

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), legacy({
+      targets: ['chrome >= 64', 'safari >= 12'],
+    modernPolyfills: true
+  })],
 })
