16c2516b39e31336c68b1314c1812bbf0d81d7dd
Lee-clipse <79911816+Lee-clipse@users.noreply.github.com>
2024-09-07 06:37:59 +0900

    modify: X 버튼 크기 증가

--- client/src/components/Popup.tsx
+++ client/src/components/Popup.tsx
@@ -139,13 +139,13 @@ const Wrapper = styled.div`
     justify-content: right;
     align-items: center;
     border-bottom: 1px solid lightgray;
-    padding: 1rem;
+    padding: 1.4rem;
 
     font-size: 1.6rem;
     font-weight: bold;
 
     img {
-      width: 5vw;
+      width: 8vw;
     }
   }
 
